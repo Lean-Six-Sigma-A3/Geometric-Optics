2c5c930f852d0577570eecd4979c04224f5fc182
N1Coder <tomofanteng123@gmail.com>
2025-03-05 22:45:10 +0800

    feat: add bodywork of cybertruck graph

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { Canvas } from './components/canvas'
-import { LineGroup } from './components/line-group'
-import { Mirror } from './components/mirror'
+import { Canvas } from "./components/canvas"
+import { LineGroup } from "./components/line-group"
+import { Mirror } from "./components/mirror"
 
-import './styles/main.css'
+import "./styles/main.css"
 
 // Ambil elemen 'canvas' yang ada di index.html
-const canvasEl = document.querySelector<HTMLCanvasElement>('#canvas')!
+const canvasEl = document.querySelector<HTMLCanvasElement>("#canvas")!
 
 // Buat object dari class Canvas
 const canvas = new Canvas({
@@ -15,7 +15,31 @@ const canvas = new Canvas({
 })
 
 // Parsing koordinat menjadi object Line (untuk ditampilkan)
-const coordinates = [[40, 0, 60, 0],[40, 20, 60, 20],[40, 0, 40, 20],[60, 10, 60, 30],[20, 40, 40, 30],[60, 30, 80, 40],[10, 50, 20, 40],[80, 40, 90, 50],[0, 50, 100, 50],[0, 50, 0, 60],[0, 50, 100, 60],[0, 60, 100, 60],[10, 60, 10, 90],[90, 60, 90, 90],[0, 90, 100, 90],[0, 90, 0, 100],[0, 90, 100, 100],[0, 100, 100, 100],[10, 100, 10, 130],[90, 100, 90, 130],[0, 90, 100, 90],[0, 90, 0, 100],[0, 90, 100, 100],[0, 100, 100, 100],[10, 100, 20, 110],[80, 110, 90, 100],[20, 110, 80, 110]]
+// Did I ever tell you the definition of insanity is?
+// This is the definition of Insanity
+// cybertruck object
+const coordinates = [
+  [145, -5, 2.5, 42.5],
+  [25, 50, 10, 80],
+  [10, 80, 2.5, 42.5],
+  [25, 50, 62.5, 50],
+  [62.5, 50, 77.5, 72.5],
+  [77.5, 72.5, 77.5, 92.5],
+  [77.5, 92.5, 235, 92.5],
+  // [235, 92.5, 235, 12.5], // cover ban belakang singgang
+  // [235, 12.5, 325, 72.5], // cover ban belakang singgang
+  [332.5, 27.5, 145, -5],
+  [332.5, 27.5, 325, 72.5],
+  [152.5, -5, 145, 35],
+  [145, 35, 145, 85],
+  [212.5, 11, 212.5, 35],
+  [212.5, 35, 212.5, 85],
+  [2.5, 42.5, 332.5, 27.5],
+  [100, 12.5, 92.5, 50],
+  [92.5, 50, 92.5, 85],
+  [92.5, 85, 212.5, 85],
+]
+
 const mirrorObject = LineGroup.fromCoordinates(coordinates)
 
 // Buat object Mirror, Mirror ini (nantinya) berisi logic utama cerminnya
@@ -23,6 +47,6 @@ const mirror = new Mirror({
   canvas: canvas,
   mirrorObject: mirrorObject,
   controlEl: {
-    distance: document.querySelector<HTMLInputElement>('#distance')!
-  }
+    distance: document.querySelector<HTMLInputElement>("#distance")!,
+  },
 })
